6f84779ecd01c59623e71c571f919ffead2b2f01
CarbonEternity <pa03622789@gmail.com>
2020-05-09 17:47:58 +0300

    windows messageBox types example

---
+++ Main.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace MessageBox
+{
+    partial class WinAPIClass
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnABORTRETRYIGNORE = new System.Windows.Forms.Button();
+            this.btnYESNOCANCEL = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnYESNO = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnABORTRETRYIGNORE
+            // 
+            this.btnABORTRETRYIGNORE.Location = new System.Drawing.Point(24, 25);
+            this.btnABORTRETRYIGNORE.Name = "btnABORTRETRYIGNORE";
+            this.btnABORTRETRYIGNORE.Size = new System.Drawing.Size(239, 23);
+            this.btnABORTRETRYIGNORE.TabIndex = 0;
+            this.btnABORTRETRYIGNORE.Text = "Прервать Повторить Пропустить";
+            this.btnABORTRETRYIGNORE.UseVisualStyleBackColor = true;
+            this.btnABORTRETRYIGNORE.Click += new System.EventHandler(this.btnABORTRETRYIGNORE_Click);
+            // 
+            // btnYESNOCANCEL
+            // 
+            this.btnYESNOCANCEL.Location = new System.Drawing.Point(24, 73);
+            this.btnYESNOCANCEL.Name = "btnYESNOCANCEL";
+            this.btnYESNOCANCEL.Size = new System.Drawing.Size(239, 23);
+            this.btnYESNOCANCEL.TabIndex = 1;
+            this.btnYESNOCANCEL.Text = "Да Нет Отмена";
+            this.btnYESNOCANCEL.UseVisualStyleBackColor = true;
+            this.btnYESNOCANCEL.Click += new System.EventHandler(this.btnYESNOCANCEL_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(24, 121);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(239, 23);
+            this.btnOK.TabIndex = 2;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnYESNO
+            // 
+            this.btnYESNO.Location = new System.Drawing.Point(24, 170);
+            this.btnYESNO.Name = "btnYESNO";
+            this.btnYESNO.Size = new System.Drawing.Size(239, 23);
+            this.btnYESNO.TabIndex = 3;
+            this.btnYESNO.Text = "Да Нет";
+            this.btnYESNO.UseVisualStyleBackColor = true;
+            this.btnYESNO.Click += new System.EventHandler(this.btnYESNO_Click);
+            // 
+            // WinAPIClass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 227);
+            this.Controls.Add(this.btnYESNO);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnYESNOCANCEL);
+            this.Controls.Add(this.btnABORTRETRYIGNORE);
+            this.Name = "WinAPIClass";
+            this.Text = "Message Box";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnABORTRETRYIGNORE;
+        private System.Windows.Forms.Button btnYESNOCANCEL;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnYESNO;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MessageBox
+{
+    public partial class WinAPIClass : Form
+    {
+        public WinAPIClass()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Вызов диалогового окна MessageBox
+        /// <summary>
+        /// Вызов диалогового окна MessageBox.
+        /// </summary>
+        /// <param name="handle">Родительская форма окна</param>
+        /// <param name="text">Текст окна.</param>
+        /// <param name="title">Заголовок окна.</param>
+        /// <param name="type">Тип окна.</param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int MessageBox(IntPtr handle, string text, string title, uint type);
+        /// <summary>
+        /// Тип диалогового окна.
+        /// </summary>
+        public enum MessageBoxType : int
+        {
+            /// <summary>
+            /// Три кнопки — Abort, Retry, Ignore
+            /// </summary>
+            MB_ABORTRETRYIGNORE = 2,
+            /// <summary>
+            /// Три кнопки — Cancel, Try, Continue
+            /// </summary>
+            MB_CANCELTRYCONTINUE = 6,
+            /// <summary>
+            /// Одна кнопка — Ok.
+            /// </summary>
+            MB_OK = 0,
+            /// <summary>
+            /// Две кнопки — Ok, Cancel.
+            /// </summary>
+            MB_OKCANCEL = 1,
+            /// <summary>
+            /// Две кнопки — Retry, Cancel
+            /// </summary>
+            MB_RETRYCANCEL = 5,
+            /// <summary>
+            /// Две кнопки — Yes, No
+            /// </summary>
+            MB_YESNO = 4,
+            /// <summary>
+            /// Три кнопки — Yes, No, Cancel
+            /// </summary>
+            MB_YESNOCANCEL = 3,
+            /// <summary>
+            /// Иконка-восклицание
+            /// </summary>
+            MB_ICONEXCLAMATION = 0x30,
+            /// <summary>
+            /// Иконка-предупреждение
+            /// </summary>
+            MB_ICONWARNING = 0x30,
+            /// <summary>
+            /// Иконка-информация
+            /// </summary>
+            MB_ICONINFORMATION = 0x40,
+            /// <summary>
+            /// Иконка-вопрос
+            /// </summary>
+            MB_ICONQUESTION = 0x20,
+            /// <summary>
+            /// Иконка-стоп
+            /// </summary>
+            MB_ICONSTOP = 0x10,
+            /// <summary>
+            /// Иконка-ошибка
+            /// </summary>
+            MB_ICONERROR = 0x10,
+
+        }
+        /// <summary>
+        /// Тип возвращаемого значения.
+        /// </summary>
+        public enum MessageBoxReturnType : int
+        {
+            IDABORT = 3,
+            IDCANCEL = 2,
+            IDCONTINUE = 11,
+            IDIGNORE = 5,
+            IDNO = 7,
+            IDOK = 1,
+            IDRETRY = 4,
+            IDTRYAGAIN = 10,
+            IDYES = 6
+        }
+        #endregion
+
+        private void btnABORTRETRYIGNORE_Click(object sender, EventArgs e)
+        {
+            //Текст сообщения
+            string text = "Hello World";
+            //Заголовок окна
+            string title = "From WinApi";
+            // Тип данных IntPtr — указатель на объект.
+            WinAPIClass.MessageBox(IntPtr.Zero, text, title, (uint)(WinAPIClass.MessageBoxType.MB_ABORTRETRYIGNORE |
+            WinAPIClass.MessageBoxType.MB_ICONQUESTION));
+        }
+
+        private void btnYESNOCANCEL_Click(object sender, EventArgs e)
+        {
+            string text = "Hello World";
+            string title = "From WinApi";
+            WinAPIClass.MessageBox(IntPtr.Zero, text, title, (uint)(WinAPIClass.MessageBoxType.MB_YESNOCANCEL |
+            WinAPIClass.MessageBoxType.MB_ICONINFORMATION));
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string text = "Hello World";
+            string title = "From WinApi";
+            WinAPIClass.MessageBox(IntPtr.Zero, text, title, (uint)(WinAPIClass.MessageBoxType.MB_OK |
+            WinAPIClass.MessageBoxType.MB_ICONSTOP));
+        }
+
+        private void btnYESNO_Click(object sender, EventArgs e)
+        {
+            string text = "Hello World";
+            string title = "From WinApi";
+            WinAPIClass.MessageBox(IntPtr.Zero, text, title, (uint)(WinAPIClass.MessageBoxType.MB_YESNO |
+            WinAPIClass.MessageBoxType.MB_ICONEXCLAMATION));
+        }
+    }
+}
